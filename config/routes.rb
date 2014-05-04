21b4fe731aa42b172106828ce3ffa02497f38f33
Christo44 <christo@bare.bz>
2014-05-04 23:48:36 +0200

    Installed devise, add a user and generate devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :statuses
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  
+   root to: 'statuses#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
